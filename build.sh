8e615c436cea30bbae9cd3a2c84e44716c79af46
sagar.varule <sagar.varule@gmail.com>
2025-11-08 05:12:58 +0000

    added build script to install flutter sdk for vercel deployment

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Exit on any error
+set -e
+
+# Clone the Flutter repository from the stable channel
+git clone https://github.com/flutter/flutter.git --depth 1 --branch stable /tmp/flutter
+
+# Add the Flutter tool to the path
+export PATH="$PATH:/tmp/flutter/bin"
+
+# Enable web support
+flutter config --enable-web
+
+# Get project dependencies
+flutter pub get
+
+# Build the Flutter web application
+flutter build web --release
